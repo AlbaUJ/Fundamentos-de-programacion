ecc871e366238d81ae82d303b916cc700bbac2a9
Alba Uréndez <albaurendez12@gmail.com>
2026-09-23 11:58:45 +0200

    ejercicio 4

---
+++ Tipos_de_datos_simples/ejercicio4.py
@@ -0,0 +1,12 @@
+
+# Ejercicio 4
+
+#Escribir un programa que muestre por pantalla el resultado de la siguiente operación
+#aritmética
+
+suma = (3+2)
+multiplicacion = (2*5)
+parentesis = (suma / multiplicacion)
+potencia = (parentesis**2)
+
+print (potencia)
