7837cc14b62d96281d196c48a9a00685608c393a
Alba Uréndez <albaurendez12@gmail.com>
2026-09-23 12:06:14 +0200

    Ejercicio 5

---
+++ Tipos_de_datos_simples/ejercicio5.py
@@ -0,0 +1,12 @@
+
+# Ejercicio 5
+
+# Escribir un programa que pregunte al usuario por el número de horas trabajadas y el
+#coste por hora. Después debe mostrar por pantalla la paga que le corresponde
+
+horas = float(input("dime el numero de horas trabajadas: "))
+coste = float(input("dime el coste por hora: "))
+
+paga = horas * coste
+
+print(paga)
