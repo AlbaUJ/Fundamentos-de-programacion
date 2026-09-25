f5afdb17be52ca18bcd652e00065b739e5d505df
Alba Uréndez <albaurendez12@gmail.com>
2026-09-25 19:31:12 +0200

    Ejercicio 10

---
+++ Tipos_de_datos_simples/ejercicio10.py
@@ -0,0 +1,21 @@
+# Ejercicio 10
+
+#Una juguetería tiene mucho éxito en dos de sus productos: payasos y muñecas.
+#Suele hacer venta por correo y la empresa de logística les cobra por peso de cada
+#paquete así que deben calcular el peso de los payasos y muñecas que saldrán en
+#cada paquete a demanda. Cada payaso pesa 112 g y cada muñeca 75 g. Escribir un
+#programa que lea el número de payasos y muñecas vendidos en el último pedido y
+#calcule el peso total del paquete que será enviado.
+
+num_payasos = int(input("dime la cantidad de payasos del pedido: "))
+num_muñecas = int(input("dime la cantidad de muñecas del pedido: "))
+
+Peso_payasos = 112 #gramos
+Peso_muñecas = 75  #gramos
+
+total_payasos = num_payasos * Peso_payasos
+total_muñecas = num_muñecas * Peso_muñecas
+
+pedido = total_payasos + total_muñecas
+
+print (f"El peso total del paquete que será enviado es de {pedido} gramos")
