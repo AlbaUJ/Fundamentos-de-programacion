7b7b5d73ebc3112e787b9b050ef7240bd7888755
Alba Uréndez <albaurendez12@gmail.com>
2026-09-25 12:38:24 +0200

    ejercicio 6

--- Tipos_de_datos_simples/ejercicio6.py
+++ Tipos_de_datos_simples/ejercicio6.py
@@ -5,3 +5,10 @@
 #después muestre en pantalla la suma de todos los enteros desde 1 hasta n. La
 #suma de los primeros enteros positivos puede ser calculada de la siguiente forma
 
+n = int(input("Dime un numero entero positivo: "))
+suma = n * (n+1) //2
+
+if n < 0 : 
+    print("tiene que ser un numero positivo")
+else:
+    print (suma)
